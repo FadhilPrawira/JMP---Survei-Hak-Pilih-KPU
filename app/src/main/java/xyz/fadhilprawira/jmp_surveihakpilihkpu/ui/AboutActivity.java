a868013de2a8331a3a4e3fb549b156c58395f9f0
Fadhil Prawira <fadhilprawira87@gmail.com>
2024-09-12 21:28:31 +0700

    Add main activity screen

---
+++ app/src/main/java/xyz/fadhilprawira/jmp_surveihakpilihkpu/ui/AboutActivity.java
@@ -0,0 +1,20 @@
+package xyz.fadhilprawira.jmp_surveihakpilihkpu.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import xyz.fadhilprawira.jmp_surveihakpilihkpu.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ app/src/main/java/xyz/fadhilprawira/jmp_surveihakpilihkpu/ui/VoterFormEntryActivity.java
@@ -0,0 +1,16 @@
+package xyz.fadhilprawira.jmp_surveihakpilihkpu.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import xyz.fadhilprawira.jmp_surveihakpilihkpu.R;
+
+public class VoterFormEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voter_form_entry);
+    }
+}
